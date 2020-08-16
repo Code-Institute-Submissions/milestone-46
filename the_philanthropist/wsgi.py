764ccae60df1f82c5b6336065cecee26de650704
an-slua-sidhe <polocoinn@yahoo.ie>
2020-08-16 14:20:46 +0100

    Fix spelling error in main project directory title

--- the_philanthropist/wsgi.py
+++ the_philanthropist/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for the_philantropist project.
+WSGI config for the_philanthropist project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_philantropist.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_philanthropist.settings')
 
 application = get_wsgi_application()
