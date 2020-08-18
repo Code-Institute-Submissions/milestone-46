7e3fc323deef1675d8d7530436dc83e6e63da718
an-slua-sidhe <polocoinn@yahoo.ie>
2020-08-18 18:56:44 +0100

    Edit admin tables and antiquities json

--- all_antiquities/admin.py
+++ all_antiquities/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Antiquity, Category
 
 
-admin.site.register(Antiquity)
-admin.site.register(Category)
+class AntiquityAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'date',
+        'period',
+        'culture',
+        'value',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Antiquity, AntiquityAdmin)
+admin.site.register(Category, CategoryAdmin)
