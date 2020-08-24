4c500cc4d3371ac8cb8f85c96505e4fe6c00a8ff
an-slua-sidhe <polocoinn@yahoo.ie>
2020-08-24 13:42:46 +0100

    Add latest_options json and make latest options page dynamic

--- latest_options/admin.py
+++ latest_options/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Latest_option
 
-# Register your models here.
+
+class Latest_optionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'value',
+        'image',
+        'antiquity_id',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Latest_option, Latest_optionAdmin)
--- latest_options/views.py
+++ latest_options/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .models import Latest_option
+
 
 def latest_options(request):
     """ This view returns the Latest Options page """
 
-    return render(request, 'latest_options/latest_options.html')
+    latest_options = Latest_option.objects.all()
+
+    context = {
+        'latest_options': latest_options,
+    }
+
+    return render(request, 'latest_options/latest_options.html', context)
