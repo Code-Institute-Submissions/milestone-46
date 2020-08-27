b6e4ea35f560155220ef9cbb59098d61ec5d3912
an-slua-sidhe <polocoinn@yahoo.ie>
2020-08-27 21:28:23 +0100

    Create forms.py in all_antiquities

---
+++ all_antiquities/forms.py
@@ -0,0 +1,19 @@
+
+from django import forms
+from .models import Antiquity, Category
+
+
+class AntiquityForm(forms.ModelForm):
+
+    class Meta:
+        model = Antiquity
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
